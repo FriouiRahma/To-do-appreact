bdb3ebffeddc272cbdcc398140654f41152e4456
FriouiRahma <friouirahma9@gmail.com>
2020-02-19 11:25:15 +0100

    To do app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+class Todoapp extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    item:"",
+    todos:[]
+  
+    }
+      
+      }
+     
+      handelChange=(e)=>{
+       this.setState({item:e.target.value})
+      }
+      handelAdd=()=>{
+       
+        this.setState({todos:this.state.todos.concat({text:this.state.item,class:false})})
+
+      }
+      handelDelete=(x)=>{
+        this.setState({todos:this.state.todos.filter(el=>el !=x)})
+      }
+      handelComplete=(text)=>{
+      this.setState({todos: this.state.todos.map(el=> el.text === text ? {...el,class : !el.class} : el)})
+      console.log(this.state.todos.class)
+      }
+
+
+
+  render() {
+    
+  
+    return (
+      <div className="gors">
+       <h3>To do app</h3>
+       <h3>Ad new To-do</h3>
+       <input type="text" name="item" onChange={this.handelChange} />
+       <button   onClick={this.handelAdd}>Add</button>
+        {this.state.todos.map((el)=><div>
+        <h1 className={el.class && "barre"}>{el.text}</h1>
+        <button onClick={()=>this.handelDelete(el)}>Delete</button>
+        <button onClick={()=>this.handelComplete(el.text)}>{el.class? "Undo":"Complete"}</button>
+
+        </div>)}
+
+      </div>
+
+    );
+
+  }
+
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Todoapp />
+
     </div>
   );
 }
